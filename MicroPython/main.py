8ce05fe0e9e37a8e7d197954c5bd6778b393254a
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-09-30 17:17:52 +0000

    changed to add cookies in python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -2,9 +2,18 @@
 Created by: Mr. Coxall
 Created on: Sep 2020
 This module is a Micro:bit MicroPython program
+This program adds then shows the number of cookies.
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+numberOfCookies = 0
+
+while True:
+    # adds one cookie
+    if button_a.is_pressed():
+        numberOfCookies = numberOfCookies + 1
+        display.show(str(numberOfCookies))
